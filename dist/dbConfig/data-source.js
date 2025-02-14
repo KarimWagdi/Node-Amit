faebcea7009bc9c664323ba1bc11abbdb30cf827
KarimWagdi <karim.wagdi@matnsolutions.com>
2025-02-14 14:08:11 +0200

    project

---
+++ dist/dbConfig/data-source.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.AppDataSource = void 0;
+require("reflect-metadata");
+const typeorm_1 = require("typeorm");
+exports.AppDataSource = new typeorm_1.DataSource({
+    type: "mysql",
+    host: "localhost",
+    port: 3306,
+    username: "root",
+    password: "Password@12345",
+    database: "market",
+    synchronize: true,
+    logging: true,
+    entities: [__dirname + "/../entity/*.js||*.ts"],
+    migrations: [],
+    subscribers: [],
+});
+// Initialize the connection
+exports.AppDataSource.initialize()
+    .then(() => {
+    console.log("Data Source has been initialized!");
+})
+    .catch((err) => {
+    console.error("Error during Data Source initialization", err);
+});
