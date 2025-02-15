b85c3df009cd3e5240f598f93af729c5065aa5f1
KarimWagdi <karim.wagdi@matnsolutions.com>
2025-02-15 15:25:06 +0200

    Data

--- src/MiddelWares/Auth.ts
+++ src/MiddelWares/Auth.ts
@@ -2,14 +2,12 @@ import { NextFunction, Response } from 'express'
 import { AppDataSource } from '../dbConfig/data-source'
 const jwt = require('jsonwebtoken')
 
-const jwtSecret = 'your-secret-key'
-
 const verifyToken = async (req: any, res: Response, next: NextFunction):Promise <any> => {
     try{
             if (req.headers && req.headers.authorization) {
                 const authorization = req.headers.authorization.split(' ')[1]
                 if (!authorization) { return res.status(403).json('A token is required for authentication') }
-                    const data = jwt.verify(authorization, jwtSecret)
+                    const data = jwt.verify(authorization, process.env.jwtSecret)
                     if (!data) return res.status(403).json({ error: 'Invalid token.' })
                     const userRepository = AppDataSource.getRepository("user");
                     const user = await userRepository.findOne({where:{ id: data.userId }});
